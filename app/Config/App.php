6454789502400acd474b1d79e84939345c8aed1c
Martin <martinwadeson@thesportsman.com>
2020-05-14 19:38:35 +0100

    Modified URL structure

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/nook.buzz/';
+	public $baseURL = 'http://localhost/nookbuzz/';
 
 	/*
 	|--------------------------------------------------------------------------

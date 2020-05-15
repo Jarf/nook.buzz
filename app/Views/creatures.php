c58e9c1dd67e3d5cf9ef86acc81b68e18069b45b
Martin <martinwadeson@thesportsman.com>
2020-05-15 15:08:57 +0100

    creature information now in popup on click

--- app/Views/creatures.php
+++ app/Views/creatures.php
@@ -4,12 +4,27 @@
 	foreach($creatures as $creature):
 		?>
 		<div class="creaturecontainer">
+			<label for="<?=$creature->sanitisedname?>">
+				<div class="creatureicon">
+					<img src="/images/NH-Icon-<?=$creature->sanitisedname?>.png"/>
+				</div>
+			</label>
+		</div>
+		<input type="radio" id="<?=$creature->sanitisedname?>" name="creatureselect" class="creaturecheckbox"/>
+		<div class="creatureinfocontainer">
+			<label class="closebutton" for="nocreatureselected">X</label>
+			<?=$creature->name?><br/>
 			<div class="creatureicon">
 				<img src="/images/NH-Icon-<?=$creature->sanitisedname?>.png"/>
 			</div>
+			<?=$creature->sell?><br/>
+			<?=$creature->size?><br/>
+			<?=$creature->location?><br/>
+			<?=$creature->timereadable?>
 		</div>
 		<?php
 	endforeach;
 	?>
+	<input type="radio" id="nocreatureselected" name="creatureselect" class="creaturecheckbox"/>
 	</div>
 </div>

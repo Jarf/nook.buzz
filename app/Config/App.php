5243d262f8087443fe742f6626802a2fd2fd91de
Martin Wadeson <martin.wadeson@gmail.com>
2020-05-15 13:50:35 +0100

    app.php changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -50,7 +50,7 @@ class App extends BaseConfig
 	|
 	| WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 	*/
-	public $uriProtocol = 'REQUEST_URI';
+	public $uriProtocol = 'PATH_INFO';
 
 	/*
 	|--------------------------------------------------------------------------

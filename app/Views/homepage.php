a910475175d2ba481360c35e2970f74cc9660743
Martin <martinwadeson@thesportsman.com>
2020-05-16 10:46:49 +0100

    Added content to homepage

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -1,5 +1,25 @@
 <div class="homepagecontainer">
 	<div class="homepage">
-		text
+		<h2>Welcome to nook.buzz</h2>
+		<h4>What is this?</h4>
+		<p>
+			Having been furloughed during this pandemic I found myself delving into Animal Crossing: New Horizons.<br/>
+			I originally created this as a simple personal tool I could leave open on my phone to help me clarify which fish/insects are worth catching in the current month,  I then expanded it to also track what I have donated to Blathers.
+		</p>
+		<h4>How do I use it?</h4>
+		<p>
+			Click one of those icons at the top to go to fish/insects for the current month.<br/>
+			Once there you will find that they are sorted from most to least valuable.<br/>
+			Clicking on a creature will bring up a dialogue providing information on the availabilty and how to catch the. You can also mark then as donated at which point a blathers stick will appear next to the picture helping you keep track of what is new.
+		</p>
+		<h4>I have feedback!</h4>
+		<p>
+			Drop me a message on <a target="_blank" href="https://twitter.com/jarfino">twitter</a> and I'll do what I can.
+		</p>
+		<h4>My thirst for knowledge is unquenched</h4>
+		<p>
+			Still here?<br/>
+			You can find the github repository <a target="_blank" href="https://github.com/Jarf/nook.buzz">here</a>.
+		</p>
 	</div>
 </div>

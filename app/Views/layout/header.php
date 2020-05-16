17817515443b146396d1058f8fd15da315ddd1f0
Martin <martinwadeson@thesportsman.com>
2020-05-16 08:27:05 +0100

    added favicon

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -8,6 +8,10 @@
 
 	<link rel="stylesheet" href="/main.css" type="text/css" media="screen">
 
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+
+
     <title>nook.buzz</title>
 </head>
 <body>

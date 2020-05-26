3ebc4b8d34b8f42299a909a5d6d7df8c3c181127
Martin Wadeson <martin.wadeson@gmail.com>
2020-05-26 15:10:45 +0100

    compile CSS into static file

--- app/Controllers/Files.php
+++ app/Controllers/Files.php
@@ -7,11 +7,18 @@ class Files extends BaseController
 	public function styles(){
 
 		$this->response->setHeader('Content-Type', 'text/css');
+		$csspath = FCPATH . 'css/main.css';
+		if(!file_exists($csspath)){
+			$scss = new Compiler();
+			$scss->addImportPath('scss');
+			$scss->setFormatter('ScssPhp\ScssPhp\Formatter\Crunched');
+			$css = $scss->compile('@import "main.scss";');
+			file_put_contents($csspath, $css);
+		}else{
+			$css = file_get_contents($csspath);
+		}
 
-		$scss = new Compiler();
-		$scss->addImportPath('scss');
-		$scss->setFormatter('ScssPhp\ScssPhp\Formatter\Crunched');
-		$css = $scss->compile('@import "main.scss";');
+
 		print $css;
 	}
 
